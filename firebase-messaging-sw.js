e8dcf50f47e76a0ffe27f394922bf7cca98dbbb0
Mehdi <elwafi.courrier99@gmail.com>
2022-09-10 00:39:28 +0100

    1er Build

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+importScripts('https://www.gstatic.com/firebasejs/9.9.4/firebase-app-compat.js');
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp({
+  apiKey: "AIzaSyCNpz_q7Q_2UeV434wkZQ_F5_ZHVuj_UYs",
+  authDomain: "safely-systems.firebaseapp.com",
+  projectId: "safely-systems",
+  storageBucket: "safely-systems.appspot.com",
+  messagingSenderId: "242820385043",
+  appId: "1:242820385043:web:1911ab0c8835617a66b2e6",
+  measurementId: "G-VT36REZM9K"
+})
